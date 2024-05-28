4a4fe3e1e6289382a183353df2ad571f8628c089
kbenlloch <kbenlloch@biit-solutions.com>
2024-05-28 15:44:06 +0200

    Fixed compilation issues

--- src/environments/environment.docker.ts
+++ src/environments/environment.docker.ts
@@ -1,6 +1,6 @@
 export class Environment {
   public static readonly ROOT_URL: string = `DOCKER:PROTOCOL://DOCKER:MACHINE_DOMAIN`;
   public static readonly APPOINTMENT_CENTER_PATH: string = 'DOCKER:APPOINTMENT_CENTER_URL';
-  public static readonly USER_MANAGER_SERVER: string = 'DOCKER:USER_MANAGER_URL';
-  public static readonly INFOGRAPHIC_ENGINE_SERVER: string = 'DOCKER:INFOGRAPHIC_ENGINE_URL';
+  public static readonly USER_MANAGER_PATH: string = 'DOCKER:USER_MANAGER_URL';
+  public static readonly INFOGRAPHIC_ENGINE_PATH: string = 'DOCKER:INFOGRAPHIC_ENGINE_URL';
 }
