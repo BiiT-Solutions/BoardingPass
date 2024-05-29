1214d6eb4d5e8158e9863e35725c674fd875fdae
kbenlloch <kbenlloch@biit-solutions.com>
2024-05-29 16:59:00 +0200

    Working scanner feature, added validation

--- src/app/views/scanner/scanner.component.ts
+++ src/app/views/scanner/scanner.component.ts
@@ -1,6 +1,8 @@
 import {AfterViewInit, Component, ViewChild} from '@angular/core';
 import {TRANSLOCO_SCOPE, TranslocoService} from "@ngneat/transloco";
 import {NgxScannerQrcodeComponent, ScannerQRCodeResult} from "ngx-scanner-qrcode";
+import {BiitProgressBarType} from "biit-ui/info";
+import {AppointmentService} from "appointment-center-structure-lib";
 
 @Component({
   selector: 'scanner',
@@ -17,34 +19,70 @@ import {NgxScannerQrcodeComponent, ScannerQRCodeResult} from "ngx-scanner-qrcode
 export class ScannerComponent implements AfterViewInit {
 
   @ViewChild('action') scanner: NgxScannerQrcodeComponent;
+  protected loading = false;
   protected success = false;
   protected error = false;
 
-  constructor(private translocoService: TranslocoService) {
+  protected readonly BiitProgressBarType = BiitProgressBarType;
+
+  constructor(private translocoService: TranslocoService,
+              private appointmentService: AppointmentService) {
   }
 
   ngAfterViewInit() {
+    console.log(this.scanner.devices)
     this.scanner.start();
   }
 
   protected scanQr(result: ScannerQRCodeResult[]) {
     this.scanner.pause();
-    let object: {appointmentId: string, attender: string};
-    try {
-      object = JSON.parse(result[0].value);
-      this.success = true;
-    } catch (e) {
-      this.error = true;
-    }
+    this.loading = true;
+
+    this.appointmentService.markAsAttendedFromQr(result[0].value).subscribe({
+      next: () => {
+        this.success = true;
+      },
+      error: () => {
+        this.error = true;
+      }
+    }).add(() => {
+      this.loading = false;
+      setTimeout(() => {
+        this.scanner.play();
+        this.clearState();
+      }, 1500);
+    });
+  }
+
+  protected scanTest(result: ScannerQRCodeResult[]) {
+    this.scanner.pause();
+    this.loading = true;
 
     setTimeout(() => {
-      this.scanner.play();
-      this.clearState();
-    }, 2000);
+      let object: {appointmentId: string, attender: string};
+      try {
+        object = JSON.parse(result[0].value);
+        this.success = true;
+      } catch (e) {
+        this.error = true;
+      }
+      this.loading = false;
+
+      setTimeout(() => {
+        this.scanner.play();
+        this.clearState();
+      }, 1500);
+    }, 1000);
   }
 
   private clearState() {
     this.success = false;
     this.error = false;
+  }
+
+  protected switchCamera() {
+    this.scanner.devices.subscribe((value) => {
+      this.scanner.playDevice(value[this.scanner.deviceIndexActive+1] ? value[this.scanner.deviceIndexActive+1].deviceId : value[0].deviceId);
+    })
   }
 }
--- src/app/views/scanner/scanner.module.ts
+++ src/app/views/scanner/scanner.module.ts
@@ -4,6 +4,7 @@ import { ScannerComponent } from './scanner.component';
 import {TranslocoRootModule} from "biit-ui/i18n";
 import {ScannerRoutingModule} from "./scanner-routing.module";
 import {LOAD_WASM, NgxScannerQrcodeModule} from "ngx-scanner-qrcode";
+import {BiitProgressBarModule} from "biit-ui/info";
 
 LOAD_WASM().subscribe();
 
@@ -14,11 +15,12 @@ LOAD_WASM().subscribe();
   exports: [
     ScannerComponent
   ],
-  imports: [
-    ScannerRoutingModule,
-    CommonModule,
-    TranslocoRootModule,
-    NgxScannerQrcodeModule
-  ]
+    imports: [
+        ScannerRoutingModule,
+        CommonModule,
+        TranslocoRootModule,
+        NgxScannerQrcodeModule,
+        BiitProgressBarModule
+    ]
 })
 export class ScannerModule { }
