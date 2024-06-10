f804541d53e5aa140020341fda3afef5eabebee6
kbenlloch <kbenlloch@biit-solutions.com>
2024-06-10 08:44:10 +0200

    fix: scanner swap camera icon

--- src/app/views/scanner/scanner.module.ts
+++ src/app/views/scanner/scanner.module.ts
@@ -5,6 +5,7 @@ import {TranslocoRootModule} from "biit-ui/i18n";
 import {ScannerRoutingModule} from "./scanner-routing.module";
 import {LOAD_WASM, NgxScannerQrcodeModule} from "ngx-scanner-qrcode";
 import {BiitProgressBarModule} from "biit-ui/info";
+import {BiitIconModule} from "biit-ui/icon";
 
 LOAD_WASM().subscribe();
 
@@ -20,7 +21,8 @@ LOAD_WASM().subscribe();
         CommonModule,
         TranslocoRootModule,
         NgxScannerQrcodeModule,
-        BiitProgressBarModule
+        BiitProgressBarModule,
+        BiitIconModule
     ]
 })
 export class ScannerModule { }
