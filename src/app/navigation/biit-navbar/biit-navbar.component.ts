5cf1991a4b566b021ae48b5753f64fb91e7e9200
kbenlloch <kbenlloch@biit-solutions.com>
2024-09-18 15:39:57 +0200

    added: logout button moved to a dropdown menu

--- src/app/navigation/biit-navbar/biit-navbar.component.ts
+++ src/app/navigation/biit-navbar/biit-navbar.component.ts
@@ -1,30 +1,46 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 import {Route, Router} from '@angular/router';
 import {provideTranslocoScope} from '@ngneat/transloco';
 import {User} from 'authorization-services-lib';
 import {SessionService} from "appointment-center-structure-lib";
+import {ContextMenuService, ContextMenuComponent} from '@perfectmemory/ngx-contextmenu'
 
 @Component({
   selector: 'biit-navbar',
   templateUrl: './biit-navbar.component.html',
   styleUrls: ['./biit-navbar.component.scss'],
-  providers: [provideTranslocoScope('navigation')]
+  providers: [provideTranslocoScope({scope:'components/navigation', alias:'t'})]
 })
 
-export class BiitNavbarComponent implements OnInit {
+export class BiitNavbarComponent implements AfterViewInit {
+  @ViewChild('contextMenu') contextMenu: ContextMenuComponent<void>;
+  @ViewChild('navUser', {read: ElementRef}) navUser: ElementRef;
   user: User;
   constructor(protected router: Router,
-              protected sessionService: SessionService) {
+              protected sessionService: SessionService,
+              private contextMenuService: ContextMenuService<void>) {
   }
 
   routes: Route[] = [];
 
-  ngOnInit() {
+  ngAfterViewInit() {
     this.user = this.sessionService.getUser();
   }
 
   log(event: any) {
     console.log("DEVELOPMENT LOG: ", event);
+  }
+
+  protected onContextMenu($event: Event): void {
+    this.contextMenuService.show(
+      this.contextMenu,
+      {
+        x: this.navUser.nativeElement.offsetLeft + this.navUser.nativeElement.offsetWidth,
+        y: this.navUser.nativeElement.offsetHeight
+      }
+    );
+    $event.preventDefault();
+    $event.stopPropagation();
   }
 }
 
