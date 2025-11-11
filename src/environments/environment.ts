2cc49907a57019a82761bdbee09d75dddb4e2302
Jorge Hortelano <jhortelano@biit-solutions.com>
2025-11-11 11:05:04 +0100

    Releasing as open source

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export class Environment {
+  public static readonly ROOT_URL: string = 'https://testing.server.com';
+  public static readonly APPOINTMENT_CENTER_SERVER: string = 'https://testing.server.com/appointment-center-backend';
+  public static readonly USER_MANAGER_SERVER: string = 'https://testing.server.com/user-manager-system-backend';
+  public static readonly INFOGRAPHIC_ENGINE_SERVER: string = 'https://testing.server.com/infographic-engine-backend';
+}
